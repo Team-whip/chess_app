9b1e9af987155bc4e8db1a60ae0b23cfc490d7e1
Tyler Pendleton <tyler.maxwell.pendleton@gmail.com>
2015-11-09 20:02:04 -0500

    Users cannot join their own game

--- app/controllers/join_games_controller.rb
+++ app/controllers/join_games_controller.rb
@@ -2,12 +2,14 @@ class JoinGamesController < ApplicationController
   before_action :authenticate_player!
 
   def create
-    if current_game.player_two_id == nil
+    if current_game.player_one_id == current_player.id
+      redirect_to :back
+    elsif current_game.player_two_id == nil
       current_player.join_games.create(game: current_game)
       current_game.update_attributes(player_two_id: current_player.id)
       redirect_to game_path(current_game)
     else
-      redirect_to :back, flash[:notice] = "Game Currently Full"
+      redirect_to :back, flash.now.alert = "Game Currently Full"
     end
   end
 
