a5723958a62df4e30ae618b44b4c15fa9962db84
Edward Heming <edwardheming@gmail.com>
2015-11-18 14:44:39 +0000

    Attempted to store x and y position but running into issues with pulling a piece id

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,12 +1,14 @@
 class PiecesController < ApplicationController
 	def select_piece
 		id = piece_params[:id].to_i
-		piece = Piece.where(x_position: x, y_position: y, game_id: @game.id)
+		x = id % 8
+		y = id / 8
+		piece = Piece.where(x_position: x, y_position: y)
+		redirect_to game_path(@game)
 	end
 
 	private
 
-	#single ID here ranging from 0-63 corresponding with the board.
 	def piece_params
 		params.require(:piece).permit(:id) 
 	end
