2cafa3755fad90f31a4cfb9c4400c22a5077a533
Edward Heming <edwardheming@gmail.com>
2015-11-30 14:56:22 +0000

    Continued castling logic

--- app/models/game.rb
+++ app/models/game.rb
@@ -20,4 +20,30 @@ class Game < ActiveRecord::Base
 		# Checks that two players are present(logged in) to play.
 	end
 
+	def legal_castle_move?(x, y, color)
+		king = Piece.find_by(type: 'King', color: color, game_id: id)
+		rook = Piece.find_by(type: 'Rook', color: color, game_id: id)
+
+		#return false if king has moved
+		#return false if rook has moved
+		#there are no pieces between the king and chosen rook
+		#the king is not in check, pass though check, or end in check
+
+		return false unless (x - king.x_position).abs == 2
+		return false unless y == king.y_position
+
+		if x > king.x_position
+			king_rook = Piece.find_by(x_position: 7, type: 'Rook', color: color, game_id: id)
+		else
+			queen_rook = Piece.find_by(x_position: 0, type: 'Rook', color: color, game_id: id)
+		end
+
+		return false if king_rook.nil?
+		return false if queen_rook.nil?
+
+		#return false unless @rook_for_castle.state == 'unmoved'
+
+		true
+	end
+
 end
