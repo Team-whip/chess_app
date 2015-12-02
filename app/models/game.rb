5aa177c0ccbf4b7e6e1172f4d9c5fbe5e06c50fe
Edward Heming <edwardheming@gmail.com>
2015-12-02 03:29:39 +0000

    Castling method drafted

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,9 +4,18 @@ class Game < ActiveRecord::Base
 	has_many :joined_players, through: :join_games, source: :player
 	belongs_to :players
 
-	def in_check?
-		# Checks to see if either player is in check.
-	end
+	def in_check?(color)
+		king = Piece.find_by(type: 'King', color: color, game_id: id )
+		enemies = Piece.where(color: 'true', game_id: id)
+	
+		enemies.each do |enemy|
+      if enemy.legal_move?(king.x_position, king.y_position)
+        @enemy_making_check = enemy
+        return true
+      end
+    end
+    false
+ 	end
 
 	def in_checkmate?
 		# Checks to see if either player is in checkmate.
@@ -20,28 +29,45 @@ class Game < ActiveRecord::Base
 		# Checks that two players are present(logged in) to play.
 	end
 
-	def legal_castle_move?(x, y, color)
-		king = Piece.find_by(type: 'King', color: color, game_id: id)
-		rook = Piece.find_by(type: 'Rook', color: color, game_id: id)
+	def legal_castle_move?(x, y, color, game_id)
+		king = Piece.find_by(type: 'King', color: color, game_id: game_id)
 
 		#return false if king has moved
-		#return false if rook has moved
-		#there are no pieces between the king and chosen rook
-		#the king is not in check, pass though check, or end in check
+		if king.color == false
+			return false unless king.x_position == 4 && king.y_position == 0
+		else
+			return false unless king.x_position == 4 && king_y_position == 7
+		end
 
+		#there are no pieces between the king and chosen rook
+		#this will be handled by pushing this through the attempt_move which checks for obstructions
+
+		#the king is not in check
+		#this will be handled by pushing this through attempt_move which checks for in_check?
+
+		#check that intent is to castle
 		return false unless (x - king.x_position).abs == 2
 		return false unless y == king.y_position
 
+		#check which side rook the castling will occur on and return false if rook has moved
+		#or been captured
 		if x > king.x_position
-			king_rook = Piece.find_by(x_position: 7, type: 'Rook', color: color, game_id: id)
-		else
-			queen_rook = Piece.find_by(x_position: 0, type: 'Rook', color: color, game_id: id)
+			king_rook = Piece.find_by(x_position: 7, type: 'Rook', color: color, game_id: game_id)
+			return false if king_rook.nil?
+			if king_rook.color == false
+				return false unless king_rook.x_position == 7 && king_rook.y_position == 0
+			elsif king_rook.color == true
+				return false unless king_rook.x_position == 7 && king_rook.y_position == 7
+			end
+		elsif x < king.x_position
+			queen_rook = Piece.find_by(x_position: 0, type: 'Rook', color: color, game_id: game_id)
+			return false if queen_rook.nil?
+			if queen_rook.color == false
+				return false unless queen_rook.x_position == 0 && queen_rook.y_position == 0
+			elsif queen_rook.color == true
+				return false unless queen_rook.x_position == 0 && queen_rook.y_position == 7
+			end
 		end
-
-		return false if king_rook.nil?
-		return false if queen_rook.nil?
-
-		#return false unless @rook_for_castle.state == 'unmoved'
 
 		true
 	end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,4 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Game, :type => :model do
+	describe "#in_check?" do
+  	before :each do
+  		@game = Game.create
+  		@board = Board.new.populate(@game.id)
+  		@board[4][3] = King.new(color: true, x_position: 4, y_position: 3)
+  		@board[4][5] = Rook.new(color: false, x_position: 4, y_position: 5)
+  		@board[5][5] = King.new(color: false, x_position: 5, y_position: 5)
+  		@king = @board[4][3]
+  		@rook = @board[4][5]
+  		@king2 = @board[5][5]
+  	end
+
+  	it "is in check" do
+  		expect(@game.in_check?(@king.color)).to be true
+  	end
+
+  	it "is not in check" do
+  		expect(@game.in_check?(@king2.color)).to be false
+  	end
+	end
+
+	describe "#legal_castle_move?" do
+  	before :each do
+  		@game = Game.create
+  		@board = Board.new.populate(@game.id)
+  		@board[4][0] = King.new(color: false, x_position: 4, y_position: 0)
+  		@king = @board[4][0]
+  	end	
+
+      context "king has not moved before" do
+  	it "king's movement is a legal castle move" do
+  		expect(@game.legal_castle_move?(@king.x_position + 2, @king.y_position, @king.color, @game.id)).to be true
+  		expect(@game.legal_castle_move?(@king.x_position - 2, @king.y_position, @king.color, @game.id)).to be true
+  	end
+  		
+      
+  	it "king's movement is not a legal castle move" do
+  		expect(@game.legal_castle_move?(@king.x_position - 3, @king.y_position, @king.color, @game.id)).to be false
+  		expect(@game.legal_castle_move?(@king.x_position + 2, @king.y_position + 1, @king.color, @game.id)).to be false
+  	end
+      end
+
+  end
 end
