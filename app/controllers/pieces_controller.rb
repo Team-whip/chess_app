cf0effc361608e072ddd4f11ffd296eb1cbdf151
Tyler Pendleton <tyler.maxwell.pendleton@gmail.com>
2015-12-13 15:29:07 -0500

    fixed turn management issues

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -9,13 +9,13 @@ class PiecesController < ApplicationController
     @board.refresh(@game_id)
     game = Game.find(@game_id)
 
-    if piece.player_id == current_player.id && game.player_turn == current_player.id
+    if piece.player_id == current_player.id && game.turn == current_player.id
       if piece.attempt_move(new_x.to_i, new_y.to_i, @board, piece.color, @game_id)
 	piece.update_attributes(x_position: new_x, y_position: new_y, moved: true)
-	if game.player_turn == game.player_one_id
-	  game.update_attributes(player_turn: game.player_two_id)
+	if game.turn == game.player_one_id
+	  game.update_attributes(turn: game.player_two_id)
 	else
-	  game.update_attributes(player_turn: game.player_one_id)
+	  game.update_attributes(turn: game.player_one_id)
 	end
 
 	render json: {
